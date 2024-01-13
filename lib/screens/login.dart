99ef2305eb9e1173bec10d032521739c14ab473b
reypryma <refanopradana@gmail.com>
2024-01-14 05:17:19 +0700

    update

--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -8,8 +8,110 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder(child: Text("Test"),);
+    double screenWidth = MediaQuery.of(context).size.width;
+    double screenHeight = MediaQuery.of(context).size.height;
+
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      body: Column(
+        children: [
+          Container(
+            height: screenHeight / 3,
+            width: screenWidth,
+            decoration: const BoxDecoration(
+                color: Colors.redAccent,
+                borderRadius:
+                    BorderRadius.only(bottomRight: Radius.circular(70))),
+            child: const Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Icon(
+                  Icons.qr_code_scanner,
+                  color: Colors.white,
+                  size: 80,
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                Text(
+                  "Attendance",
+                  style: TextStyle(
+                      fontSize: 25,
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold),
+                ),
+                SizedBox(
+                  height: 50,
+                ),
+              ],
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(20),
+            child: Column(
+              children: [
+                TextField(
+                  decoration: const InputDecoration(
+                    labelText: "Employee Email ID",
+                    prefixIcon: Icon(Icons.person),
+                    border: OutlineInputBorder(),
+                  ),
+                  controller: _emailController,
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                TextField(
+                  decoration: const InputDecoration(
+                    labelText: "Password",
+                    prefixIcon: Icon(Icons.lock),
+                    border: OutlineInputBorder(),
+                  ),
+                  controller: _passwordController,
+                  obscureText: true,
+                ),
+                const SizedBox(
+                  height: 30,
+                ),
+                const SizedBox(
+                  height: 60,
+                  width: double.infinity,
+                  child: Center(
+                    child: CircularProgressIndicator(),
+                  ),
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    // Your login logic here
+                  },
+                  style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.redAccent,
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(30))),
+                  child: const Text(
+                    "LOGIN",
+                    style: TextStyle(fontSize: 20),
+                  ),
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                TextButton(
+                  onPressed: () {
+                    // Your registration logic here
+                  },
+                  child: const Text("Are you a new Employee? Register here"),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
