a065ae6814f62be574cdc5212adf2b0c3f573a9a
reypryma <refanopradana@gmail.com>
2024-01-21 21:21:37 +0700

    init models and add utils

---
+++ lib/models/attendance_model.dart
@@ -0,0 +1,31 @@
+class AttendanceModel {
+  final String id;
+  final String date;
+  final String checkIn;
+  final String? checkOut;
+  final DateTime createdAt;
+  final Map? checkInLocation;
+  final Map? checkOutLocation;
+
+  AttendanceModel(
+      {required this.id,
+      required this.date,
+      required this.checkIn,
+      required this.checkOut,
+      required this.createdAt,
+      required this.checkInLocation,
+      required this.checkOutLocation});
+
+  factory AttendanceModel.fromJson(Map<String, dynamic> data){
+    return AttendanceModel(
+      id: data['employee_id'],
+      date: data['date'],
+      checkIn: data['check_in'],
+      checkOut: data['check_out'],
+      createdAt: DateTime.parse(data['created_at']),
+      checkInLocation: data['check_in_location'],
+      checkOutLocation: data['check_out_location'],
+    );
+  }
+
+}
