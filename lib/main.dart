b4676d914862caf196e4792b7be5d0a85f200bf2
reypryma <refanopradana@gmail.com>
2024-01-21 21:21:18 +0700

    init services and auth providers

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
+import 'package:attendance_staff/providers/auth_provider.dart';
 import 'package:attendance_staff/screens/login_screen.dart';
+import 'package:attendance_staff/services/db_service.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
+import 'package:provider/provider.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -18,13 +21,19 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Attendance',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (context) => DBService()),
+        ChangeNotifierProvider(create: (context) => AuthProvider()),
+      ],
+      child: MaterialApp(
+        title: 'Flutter Attendance',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          useMaterial3: true,
+        ),
+        home: const LoginScreen(),
       ),
-      home: const LoginScreen(),
     );
   }
 }
---
+++ lib/providers/auth_provider.dart
@@ -0,0 +1,62 @@
+import 'package:attendance_staff/helper/utills.dart';
+import 'package:attendance_staff/services/db_service.dart';
+import 'package:flutter/material.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+
+abstract class IAuthProvider {
+  Future registerEmployee(String email, String password, BuildContext context);
+
+  Future loginEmployee(String email, String password, BuildContext context);
+
+  Future signOut();
+}
+
+class AuthProvider extends ChangeNotifier implements IAuthProvider {
+  final SupabaseClient _supabase = Supabase.instance.client;
+  final DBService _dbService = DBService();
+
+  bool _isLoading = false;
+
+  bool get isLoading => _isLoading;
+
+  set isLoading(bool value) {
+    _isLoading = value;
+  }
+
+  @override
+  Future loginEmployee(String email, String password, BuildContext context) {
+    // TODO: implement loginEmployee
+    throw UnimplementedError();
+  }
+
+  @override
+  Future registerEmployee(
+      String email, String password, BuildContext context) async {
+    try {
+      isLoading = true;
+      if (email == "" || password == "") {
+        throw ("All Fields are required");
+      }
+      final AuthResponse response =
+          await _supabase.auth.signUp(email: email, password: password);
+      await _dbService.insertNewUser(email, response.user!.id);
+
+      if (context.mounted) {
+        Utils.showSnackBar("Successfully registered !", context,
+            color: Colors.green);
+        await loginEmployee(email, password, context)
+            .then((value) => Navigator.pop(context));
+      }
+    } catch (e) {
+      isLoading = false;
+      if (!context.mounted) return;
+      Utils.showSnackBar(e.toString(), context, color: Colors.red);
+    }
+  }
+
+  @override
+  Future signOut() {
+    // TODO: implement signOut
+    throw UnimplementedError();
+  }
+}
