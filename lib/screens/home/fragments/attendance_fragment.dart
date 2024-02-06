655366bafd2ba1fa0109e5e24fce2f0df982f995
reypryma <refanopradana@gmail.com>
2024-02-06 07:47:59 +0700

    add homescreen view

---
+++ lib/screens/home/fragments/attendance_fragment.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AttendanceFragment extends StatefulWidget {
+  const AttendanceFragment({Key? key}) : super(key: key);
+
+  @override
+  State<AttendanceFragment> createState() => _AttendanceFragmentState();
+}
+
+class _AttendanceFragmentState extends State<AttendanceFragment> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
