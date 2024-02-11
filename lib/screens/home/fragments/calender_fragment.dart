8f265c1de705e63dcc6d974734dec619072986b1
reypryma <refanopradana@gmail.com>
2024-02-11 16:30:39 +0700

    implement calendar

--- lib/screens/home/fragments/calender_fragment.dart
+++ lib/screens/home/fragments/calender_fragment.dart
@@ -1,3 +1,4 @@
+import 'package:attendance_staff/models/attendance_model.dart';
 import 'package:attendance_staff/providers/attendance_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
@@ -44,26 +45,120 @@ class _CalenderFragmentState extends State<CalenderFragment> {
                 child: const Text("Pick a month")),
           ],
         ),
-        FutureBuilder(
-          future: attendanceProvider.getAttendanceHistory(),
-          builder: (context, snapshot) {
-            if (snapshot.hasData) {
-              if (snapshot.data!.isNotEmpty) {
-                return Container();
-              } else {
-                return const Center(
-                  child: Text(
-                    "No Data Available",
-                    style: TextStyle(fontSize: 25),
-                  ),
-                );
+        Expanded(
+          child: FutureBuilder(
+            future: attendanceProvider.getAttendanceHistory(),
+            builder: (context, AsyncSnapshot<List<AttendanceModel>> snapshot) {
+              if (snapshot.hasData) {
+                if (snapshot.data!.isNotEmpty) {
+                  return ListView.builder(
+                    itemCount: snapshot.data!.length,
+                    itemBuilder: (context, index) {
+                      AttendanceModel attendanceData = snapshot.data![index];
+                      return Container(
+                        margin: const EdgeInsets.only(
+                            top: 12, left: 20, right: 20, bottom: 10),
+                        height: 150,
+                        decoration: const BoxDecoration(
+                            color: Colors.white,
+                            boxShadow: [
+                              BoxShadow(
+                                  color: Colors.black26,
+                                  blurRadius: 10,
+                                  offset: Offset(2, 2)),
+                            ],
+                            borderRadius: BorderRadius.all(Radius.circular(20))),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                            Expanded(
+                              child: Container(
+                                decoration: const BoxDecoration(
+                                  color: Colors.redAccent,
+                                  borderRadius:
+                                      BorderRadius.all(Radius.circular(20)),
+                                ),
+                                child: Center(
+                                  child: Text(
+                                    DateFormat("EE \n dd")
+                                        .format(attendanceData.createdAt),
+                                    style: const TextStyle(
+                                        fontSize: 18,
+                                        color: Colors.white,
+                                        fontWeight: FontWeight.bold),
+                                  ),
+                                ),
+                              ),
+                            ),
+                            Expanded(
+                                child: Column(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  crossAxisAlignment:
+                                  CrossAxisAlignment.center,
+                                  children: [
+                                    const Text(
+                                      "Check in",
+                                      style: TextStyle(
+                                          fontSize: 20,
+                                          color: Colors.black54),
+                                    ),
+                                    const SizedBox(
+                                      width: 80,
+                                      child: Divider(),
+                                    ),
+                                    Text(
+                                      attendanceData.checkIn,
+                                      style: const TextStyle(fontSize: 25),
+                                    )
+                                  ],
+                                )),
+                            Expanded(
+                                child: Column(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  crossAxisAlignment:
+                                  CrossAxisAlignment.center,
+                                  children: [
+                                    const Text(
+                                      "Check Out",
+                                      style: TextStyle(
+                                          fontSize: 20,
+                                          color: Colors.black54),
+                                    ),
+                                    const SizedBox(
+                                      width: 80,
+                                      child: Divider(),
+                                    ),
+                                    Text(
+                                      attendanceData.checkOut?.toString() ??
+                                          '--/--',
+                                      style: const TextStyle(fontSize: 25),
+                                    )
+                                  ],
+                                )),
+                            const SizedBox(
+                              width: 15,
+                            )
+                          ],
+                        ),
+                      );
+                    },
+                  );
+                } else {
+                  return const Center(
+                    child: Text(
+                      "No Data Available",
+                      style: TextStyle(fontSize: 25),
+                    ),
+                  );
+                }
               }
-            }
-            return const LinearProgressIndicator(
-              backgroundColor: Colors.white,
-              color: Colors.grey,
-            );
-          },
+              return const LinearProgressIndicator(
+                backgroundColor: Colors.white,
+                color: Colors.grey,
+              );
+            },
+          ),
         )
       ],
     );
